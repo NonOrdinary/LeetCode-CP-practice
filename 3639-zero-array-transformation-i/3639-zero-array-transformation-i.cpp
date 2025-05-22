e9c531f7d412883cde3f891a784559d19b4aaa8a
NonOrdinary <134141566+NonOrdinary@users.noreply.github.com>
2025-05-22 22:31:15 +0530

    3639-ZERO-ARRAY-TRANSFORMATION-I Stats: Time: 0 ms (100%), Space: 293 MB (53.14%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        //the problem is standard problem as difference array,
+        //i might try to optimise a little bit.
         vector<int>diff(nums.size()+1,0);
-        for(auto &i:queries){
-            diff[i[0]]--;
-            diff[i[1]+1]++;
+        for(int i=0;i<queries.size();i++){
+            diff[queries[i][0]]++;
+            diff[queries[i][1]+1]--;
         }
         for(int i=1;i<diff.size();i++){
             diff[i]+=diff[i-1];
         }
-        
-        for(int i=0;i<diff.size()-1;i++){
-            diff[i]+=nums[i];
-            if(diff[i]>0){
+
+        for(int i=0;i<nums.size();i++){
+            if(diff[i]<nums[i]){
                 return false;
             }
         }
+
         return true;
     }
 };
